47a069d527dfa5fb71435decd37f352b6f3a493f
Ho-Sheng Hsiao <hosh@opscode.com>
2013-09-09 17:09:09 -0700

    CHEF-4086: Added base_resource_url and s3_external_url configuration
    By default base_resource_url is the atom 'host_header'. In this setting,
    URL generation returned to the requestor will be based upon the value for
    the Host header in the request.
    
    You can set a string value for base_resource_url to override the URL
    being sent back to the requestor. For example, "https://api.example.com"
    
    Likewise, s3_external_url controls the presigned URLs for cookbook
    artifacts. By default, it is set to the atom 'host_header' and will use
    the Host header from the request to generate the presigned url.
    
    This can also take a string value.
    
    When configuring for S3 instead of bookshelf, this value should be set
    to the same value found in s3_url.

--- files/chef-server-cookbooks/chef-server/libraries/helper.rb
+++ files/chef-server-cookbooks/chef-server/libraries/helper.rb
@@ -122,5 +122,16 @@ EOKEY
         OpenSSL::PKey::RSA.new(keypair_string)
       end
   end
+
+  def self.erl_atom_or_string(term)
+    case term
+    when Symbol
+      term
+    when String
+      "\"#{term}\""
+    else
+      "undefined"
+    end
+  end
 end
 
