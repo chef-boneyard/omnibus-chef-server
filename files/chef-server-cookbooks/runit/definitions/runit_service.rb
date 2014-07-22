4d513feebcd04d3a15885717c90db8e5020aeed8
Thomas Berger <th.berger@it.piratenpartei.de>
2014-07-23 00:15:40 +0200

    Fix runit-cookbook for gentoo

--- files/chef-server-cookbooks/runit/definitions/runit_service.rb
+++ files/chef-server-cookbooks/runit/definitions/runit_service.rb
@@ -152,10 +152,8 @@ define :runit_service, :directory => nil, :only_if => false, :finish_script => f
       end
     end
 
-    unless node[:platform] == "gentoo"
-      link service_dir_name do
-        to sv_dir_name
-      end
+    link service_dir_name do
+      to sv_dir_name
     end
 
     ruby_block "supervise_#{params[:name]}_sleep" do
